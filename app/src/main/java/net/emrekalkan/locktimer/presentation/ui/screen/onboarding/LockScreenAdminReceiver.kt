eab6373b53695e1f5e1cf9ff29ff2be7d9db96df
Emre Kalkan <emrekkalkan@gmail.com>
2023-01-21 15:49:41 +0300

    Update admin receiver labels

--- app/src/main/java/net/emrekalkan/locktimer/presentation/ui/screen/onboarding/LockScreenAdminReceiver.kt
+++ app/src/main/java/net/emrekalkan/locktimer/presentation/ui/screen/onboarding/LockScreenAdminReceiver.kt
@@ -1,36 +1,23 @@
 package net.emrekalkan.locktimer.presentation.ui.screen.onboarding
 
 import android.app.admin.DeviceAdminReceiver
-import android.app.admin.DevicePolicyManager
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.plus
-import javax.inject.Inject
+import net.emrekalkan.locktimer.R
 
 class LockScreenAdminReceiver : DeviceAdminReceiver() {
 
-    private val scope = CoroutineScope(Dispatchers.Default) + SupervisorJob()
-
-    private val _adminEvents = MutableSharedFlow<DeviceAdminEvents>()
-    val adminEvents: SharedFlow<DeviceAdminEvents> = _adminEvents
-
     override fun onEnabled(context: Context, intent: Intent) {
-        context.showToast("Admin enabled")
+        context.showToast(context.getString(R.string.admin_permission_enabled))
     }
 
     override fun onDisabled(context: Context, intent: Intent) {
-        context.showToast("Sample Device Admin: disabled")
+        context.showToast(context.getString(R.string.admin_permission_disabled))
     }
 
     override fun onDisableRequested(context: Context, intent: Intent): CharSequence {
-        return "Pls do not disable..."
+        return context.getString(R.string.admin_disable_request_description)
     }
 
     private fun Context.showToast(text: String) {
