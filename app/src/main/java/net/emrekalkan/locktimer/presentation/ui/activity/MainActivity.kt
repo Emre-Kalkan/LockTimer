f2e8483658209addb29db398d358c85cab599561
Emre Kalkan <emrekkalkan@gmail.com>
2023-03-29 15:20:31 +0300

    Remove unused files

--- app/src/main/java/net/emrekalkan/locktimer/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/net/emrekalkan/locktimer/presentation/ui/activity/MainActivity.kt
@@ -2,30 +2,19 @@ package net.emrekalkan.locktimer.presentation.ui.activity
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.staticCompositionLocalOf
 import dagger.hilt.android.AndroidEntryPoint
 import net.emrekalkan.locktimer.presentation.ui.base.BaseActivity
-import net.emrekalkan.locktimer.presentation.util.countdown.CountDownAlarmScheduler
 import net.emrekalkan.locktimer.presentation.ui.screen.base.LockTimer
 import net.emrekalkan.locktimer.presentation.ui.theme.LockTimerTheme
-import javax.inject.Inject
-
-val LocalCountDownAlarmSchedulerProvider = staticCompositionLocalOf<CountDownAlarmScheduler> { error("No scheduler provided.") }
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
 
-    @Inject
-    lateinit var countDownAlarmScheduler: CountDownAlarmScheduler
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CompositionLocalProvider(LocalCountDownAlarmSchedulerProvider provides countDownAlarmScheduler) {
-                LockTimerTheme {
-                    LockTimer()
-                }
+            LockTimerTheme {
+                LockTimer()
             }
         }
     }
